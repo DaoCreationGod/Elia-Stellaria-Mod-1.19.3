1707bb9f94db09d5e4870e6262420599c61dd7ae
HentaiMaster <gabriel.03@live.se>
2023-05-12 23:42:41 +0200

    second update git

--- src/main/java/net/daocreationgod/eliastellariamod/EliaStellariaMod.java
+++ src/main/java/net/daocreationgod/eliastellariamod/EliaStellariaMod.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//Update
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(EliaStellariaMod.MOD_ID)
 public class EliaStellariaMod {
